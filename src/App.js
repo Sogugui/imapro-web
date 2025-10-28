296668312b10dcd30f3914f6072d20dd1b6101a2
Sofia Guardia <sofia.guardia@mayoral.com>
2025-10-28 17:59:35 +0100

    fix: unused imports

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-import {BrowserRouter} from 'react-router-dom';
-import Header from './components/Header/Header';
-import Main from './components/Main/Main';
+import "./App.css";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
 // import Footer from './components/Footer/Footer'
 
 function App() {
   return (
     <div className="App">
-     <BrowserRouter>
-      <Header/>
-      <Main/>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Header />
+        <Main />
+      </BrowserRouter>
     </div>
   );
 }
